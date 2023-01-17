fa5783231b1982f86aed8cdd3b991430a908d9d2
snilsnil <heo6601@naver.com>
2023-01-18 07:30:10 +0900

    1일차

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kotlin
+Kotlin 개발자가 쓴 책의 번역본인 '코틀린 완벽 가이드'와 'Do it kotlin programming'책을 보며 공부
+
+ ## 일수
+ - 1일차: 'Do it kotlin programming'중 2-2 변수와 자료형 까지(p.68)
---
+++ src/main/kotlin/chap02/section2/StringExpression.kt
@@ -0,0 +1,10 @@
+package chap02.section2
+
+fun main() {
+    var a=1
+    var str1 = "a=$a"
+    var str2 = "a=${a + 3}"
+
+    println("str1 \"$str1\", str2: \"$str2\"")
+
+}
